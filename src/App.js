a032e36224a25729e18a084f49e5a65c63e1875b
Pallavi <pallavijorapur20@gmail.com>
2021-12-18 00:37:16 -0700

    random quote machine final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './style.css';
+import React, {useState, useEffect} from 'react';
+
+
+
+function getQuote(){
+  var x= Math.floor(Math.random()*(15-1)+1);
+  return fetch(
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+  )
+  .then((response) => response.json())
+  .then((responseData) => {
+    console.log(responseData['quotes'][x]);
+    return (responseData['quotes'][x]);
+    })
+
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+
+const App = () => {
+
+  const [text, setText] = useState('');
+
+  const [author, setAuthor] = useState('');
+  
+  useEffect(() => {
+
+    getQuote().then((data) => {
+      setText(data.quote);
+      setAuthor(data.author);
+    });
+  }, []);
+
+  const newQuote = () =>{
+    getQuote().then((data) => {
+      setText(data.quote);
+      setAuthor(data.author);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className='main'>
+      <div id="quote-box">
+        <p id="text">{'"'}{text}</p>
+        <h2 id="author">-{' '}{author}</h2>
+      <div className='actions'>
+        <button id="new-quote" onClick={e => newQuote()}>New quote</button>
+        <a id="tweet-quote" href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${text}`}>
+          <i className="fab fa-twitter"></i>
         </a>
-      </header>
+        
+        </div>
+
+      </div>
+      </div>
     </div>
   );
 }
